a3feb81da83180ed1a43bed211134c853e057a13
Bartosz Karczmarczyk <bbkaczy@gmail.com>
2021-09-09 22:31:50 +0200

    activeLink

--- js/script.js
+++ js/script.js
@@ -3,4 +3,37 @@ document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   });
+
+  'use strict';
+  event.preventDefault();
+  const titleClickHandler = function(){
+    console.log('Link was clicked!');
+
+/*[DONE] remove class 'active' from all article links  */
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+
+  /*[DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.titles a.active');
+
+for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+}
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+  }
+  const links = document.querySelectorAll('.titles a');
   
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
