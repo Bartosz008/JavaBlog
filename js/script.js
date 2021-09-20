ad0a77cb6454ccc052e234b534212ce893ba9bca
Bartosz Karczmarczyk <bbkaczy@gmail.com>
2021-09-20 11:25:36 +0200

    remove class active js

--- js/script.js
+++ js/script.js
@@ -38,11 +38,19 @@ const titleClickHandler = function(){
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('active');
 
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -51,6 +59,7 @@ const titleClickHandler = function(){
 }
 
 const links = document.querySelectorAll('.titles a');
+console.log(link);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
