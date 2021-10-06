512e2ac566210c70065f07010c2a8081406c18cc
Bartosz Karczmarczyk <bbkaczy@gmail.com>
2021-10-06 22:37:24 +0200

    fix html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-const templates = {
-  articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML)
-}
 const titleClickHandler = function(event){
   event.preventDefault();
   const clickedElement = this;
@@ -259,4 +256,78 @@ generateTags();
 addClickListenersToTags();
 generateAuthors();
 addClickListenersToAuthor();
+
+function optTagsListSelector (tags,list);
+optCloudClassCount (5);
+optCloudClassPrefix  (tag-size);
+calculateTagClass (count, params);
+
+params.max = Math.max(tags[tag], params.max);
+return params;
+
+const classNumber = Math.floor( percentage * (optCloudClassCount - 1) + 1 );
+
+ generateTags()
+
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
+
+  /* find all articles */
+  const articles = document.querySelectorAll('.post')
+
+  /* START LOOP: for every article: */
+  for (let article of articles){
+
+    /* find tags wrapper */
+const tagsWrapperList = article.querySelector('.post-tags .list');
+
+    /* make html variable with empty string */
+    let html = '';
+
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+
+    /* START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+
+      /* generate HTML of the link */
+      const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
+      console.log(linkHTML);
+
+      /* add generated code to html variable */
+      html = html + linkHTML;
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if(!allTags[tag]) {
+        /* [NEW] add generated code to allTags array */
+        allTags[tag] = 1;
+      }else{
+        allTags[tag]++;
+      }
+
+    /* END LOOP: for each tag */
+    }
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+  }
+  generateAuthors 
+ /* [NEW] create variable for all links HTML code */
+ const authorsParams = calculateAuthorsParams(allAuthors);
+console.log('authorParams:', authorsParams)
+let allTagsHTML = '';
+
+/* [NEW] START LOOP: for each tag in allAuthors: */
+for(let author in allAuthors){
+  /* [NEW] generate code of a link and add it to allAuthorsHTML */
+  const authorLinkHTML = '<li>' + calculateAuthorsClass(allAuthors[author], authorParam) + '</li>';
+console.log('authorLinkHTML:', authorLinkHTML);
+  allAuthorsHTML += authorLinkHTML;
+/* [NEW] END LOOP: for each tag in allAuthors: */
+}
+/*[NEW] add HTML from allAuthorsHTML to authorList */
+authorList.innerHTML = allAuthorsHTML;
 
